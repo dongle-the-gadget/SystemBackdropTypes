bed29bff4eba2d4495885af35b423c80fda2ddaf
Dongle <programcake@gmail.com>
2021-12-16 17:57:37 +0700

    Add project files.

---
+++ SystemBackdropTypes/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SystemBackdropTypes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (Environment.OSVersion.Version.Build < 22523)
+            {
+                MessageBox.Show("This demonstration requires Windows 11 Insider Preview build 22523 or newer.", "Incompatible Windows build", MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown();
+                return;
+            }
+            new MainWindow().Show();
+        }
+    }
+}
---
+++ SystemBackdropTypes/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SystemBackdropTypes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [Flags]
+        enum DWM_SYSTEMBACKDROP_TYPE
+        {
+            DWMSBT_MAINWINDOW = 2,
+            DWMSBT_TRANSIENTWINDOW = 3,
+            DWMSBT_TABBEDWINDOW = 4
+        }
+
+        [Flags]
+        enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+            DWMWA_SYSTEMBACKDROP_TYPE = 38
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MARGINS
+        {
+            public int cxLeftWidth;      // width of left border that retains its size
+            public int cxRightWidth;     // width of right border that retains its size
+            public int cyTopHeight;      // height of top border that retains its size
+            public int cyBottomHeight;   // height of bottom border that retains its size
+        };
+
+        [DllImport("DwmApi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(
+            IntPtr hwnd,
+            ref MARGINS pMarInset);
+
+        [DllImport("dwmapi.dll")]
+        static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE dwAttribute, ref int pvAttribute, int cbAttribute);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            RefreshFrame();
+            RefreshDarkMode();
+            SizeChanged += (_, _) => RefreshFrame();
+            ModernWpf.ThemeManager.Current.ActualApplicationThemeChanged += (_, _) => RefreshDarkMode();
+        }
+
+        private void RefreshFrame()
+        {
+            IntPtr mainWindowPtr = new WindowInteropHelper(this).Handle;
+            HwndSource mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
+            mainWindowSrc.CompositionTarget.BackgroundColor = Color.FromArgb(0, 0, 0, 0);
+
+            System.Drawing.Graphics desktop = System.Drawing.Graphics.FromHwnd(mainWindowPtr);
+            float DesktopDpiX = desktop.DpiX;
+
+            MARGINS margins = new MARGINS();
+            margins.cxLeftWidth = Convert.ToInt32(5 * (DesktopDpiX / 96));
+            margins.cxRightWidth = Convert.ToInt32(5 * (DesktopDpiX / 96));
+            margins.cyTopHeight = Convert.ToInt32(((int)ActualHeight + 5) * (DesktopDpiX / 96));
+            margins.cyBottomHeight = Convert.ToInt32(5 * (DesktopDpiX / 96));
+
+            DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
+        }
+
+        private void RefreshDarkMode()
+        {
+            var isDark = ModernWpf.ThemeManager.Current.ActualApplicationTheme == ModernWpf.ApplicationTheme.Dark;
+            int flag = isDark ? 1 : 0;
+            DwmSetWindowAttribute(new WindowInteropHelper(this).Handle, DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE, ref flag, Marshal.SizeOf<int>());
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            int flag = int.Parse((string)((RadioButton)sender).Tag);
+            DwmSetWindowAttribute(new WindowInteropHelper(this).Handle, DWMWINDOWATTRIBUTE.DWMWA_SYSTEMBACKDROP_TYPE, ref flag, Marshal.SizeOf<int>());
+        }
+    }
+}
